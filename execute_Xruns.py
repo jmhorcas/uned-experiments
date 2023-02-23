c9bd702b9c1cdd91f09e953c6fddf0bb94a7feb0
José Miguel Horcas <horcas@uma.es>
2023-02-23 14:34:02 +0100

    avoid already analyzed models

--- execute_Xruns.py
+++ execute_Xruns.py
@@ -22,13 +22,13 @@ def get_fm_filepath_models(dir: str) -> list[str]:
         for file in files:
             filepath = os.path.join(root, file)
             models.append(filepath)
-    return models
+    return sorted(models)
 
 
 def main(runs: int, filepath: str, solver_name: str, command: list[str]) -> tuple[str, list[str]]:
     # Get path and filename
     path, filename = os.path.split(filepath)
-    filename = filename.split('.')[0]
+    filename = filename.split('.')[:-1]
 
     results = []
     print(f'Executing {runs} runs for model {filepath} with solver {solver_name}:')
@@ -60,6 +60,9 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--dir', dest='dir', type=str, required=True, help='Folder with the models in Dimacs (.cnf).')
     args = parser.parse_args()
 
+    # to avoid already analyzed models
+    with open('result1.csv', 'r') as f:
+        analyzed_models = f.read()
 
     n_runs = args.runs
     all_models = get_fm_filepath_models(args.dir)
@@ -67,41 +70,48 @@ if __name__ == '__main__':
     header_file = None
     for i, filepath in enumerate(all_models, 1):
         print(f'FM {i}/{n_models} ({round(i/n_models*100, 2)}%): {filepath}')
-        try:
-            # Glucose4 solver
-            solver_name = 'glucose4'
-            header, result = main(n_runs, filepath, solver_name, ['python', SCRIPT_PYTHON, '-fm', filepath, '-s', solver_name])
-            if header_file is None:
-                header_file = header
-                with open(OUTPUT_FILE, 'w+', encoding='utf8') as file:
-                    file.write(f'{header_file}{os.linesep}')
-            with open(OUTPUT_FILE, 'a', encoding='utf8') as file:
+
+        # Avoid already analyzed models
+        path, filename = os.path.split(filepath)
+        filename = filename.split('.')[:-1]
+        if filename in analyzed_models:
+            print(f'Skypped model.')
+        else:
+            try:
+                # Glucose4 solver
+                solver_name = 'glucose4'
+                header, result = main(n_runs, filepath, solver_name, ['python', SCRIPT_PYTHON, '-fm', filepath, '-s', solver_name])
+                if header_file is None:
+                    header_file = header
+                    with open(OUTPUT_FILE, 'w+', encoding='utf8') as file:
+                        file.write(f'{header_file}{os.linesep}')
+                with open(OUTPUT_FILE, 'a', encoding='utf8') as file:
+                        file.write(f'{os.linesep.join(result)}{os.linesep}')
+                
+                # lingeling solver
+                solver_name = 'lingeling'
+                _, result = main(n_runs, filepath, solver_name, ['python', SCRIPT_PYTHON, '-fm', filepath, '-s', solver_name])
+                with open(OUTPUT_FILE, 'a', encoding='utf8') as file:
+                        file.write(f'{os.linesep.join(result)}{os.linesep}')
+
+                # minisat22 solver
+                solver_name = 'minisat22'
+                _, result = main(n_runs, filepath, solver_name, ['python', SCRIPT_PYTHON, '-fm', filepath, '-s', solver_name])
+                with open(OUTPUT_FILE, 'a', encoding='utf8') as file:
                     file.write(f'{os.linesep.join(result)}{os.linesep}')
-            
-            # lingeling solver
-            solver_name = 'lingeling'
-            _, result = main(n_runs, filepath, solver_name, ['python', SCRIPT_PYTHON, '-fm', filepath, '-s', solver_name])
-            with open(OUTPUT_FILE, 'a', encoding='utf8') as file:
+
+                # sat4j solver (FeatureIDE)
+                path, filename = os.path.split(filepath)
+                filename = filename.split('.')[0]
+                filepath_dimacs = os.path.join(path, filename + '.dimacs')
+                print(f'DIMACS file: {filepath_dimacs}')
+                solver_name = 'sat4j'
+                _, result = main(n_runs, filepath_dimacs, solver_name, ['java', '-jar', SCRIPT_JAVA, filepath_dimacs])
+                with open(OUTPUT_FILE, 'a', encoding='utf8') as file:
                     file.write(f'{os.linesep.join(result)}{os.linesep}')
 
-            # minisat22 solver
-            solver_name = 'minisat22'
-            _, result = main(n_runs, filepath, solver_name, ['python', SCRIPT_PYTHON, '-fm', filepath, '-s', solver_name])
-            with open(OUTPUT_FILE, 'a', encoding='utf8') as file:
-                file.write(f'{os.linesep.join(result)}{os.linesep}')
 
-            # sat4j solver (FeatureIDE)
-            path, filename = os.path.split(filepath)
-            filename = filename.split('.')[0]
-            filepath_dimacs = os.path.join(path, filename + '.dimacs')
-            print(f'DIMACS file: {filepath_dimacs}')
-            solver_name = 'sat4j'
-            _, result = main(n_runs, filepath_dimacs, solver_name, ['java', '-jar', SCRIPT_JAVA, filepath_dimacs])
-            with open(OUTPUT_FILE, 'a', encoding='utf8') as file:
-                file.write(f'{os.linesep.join(result)}{os.linesep}')
-
-
-        except Exception as e:
-            print(f'Error in model: {filepath}')
-            print(e)
+            except Exception as e:
+                print(f'Error in model: {filepath}')
+                print(e)
     
