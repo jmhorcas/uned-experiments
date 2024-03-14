5883bb4f34c4f3542c85301b8d9a54809e6cf869
José Miguel Horcas <horcas@uma.es>
2024-03-14 13:40:52 +0100

    add more info about exceptions

--- execute_Xruns.py
+++ execute_Xruns.py
@@ -144,5 +144,5 @@ if __name__ == '__main__':
 
             except Exception as e:
                 print(f'Error in model: {filepath}')
-                print(e)
+                print(e.with_traceback())
     
