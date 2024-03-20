d5d48b5ec56527758c1d5e5df2b8d7af20aee635
José Miguel Horcas <horcas@uma.es>
2024-03-20 13:33:24 +0100

    Update models and SAT4J

--- java/UNED_experiments/src/tests/Main.java
+++ java/UNED_experiments/src/tests/Main.java
@@ -2,9 +2,11 @@ package tests;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 import de.ovgu.featureide.fm.core.FeatureModelAnalyzer;
 import de.ovgu.featureide.fm.core.analysis.cnf.formula.FeatureModelFormula;
+import de.ovgu.featureide.fm.core.base.IFeature;
 import de.ovgu.featureide.fm.core.base.IFeatureModel;
 import de.ovgu.featureide.fm.core.init.FMCoreLibrary;
 import de.ovgu.featureide.fm.core.init.LibraryManager;
@@ -17,7 +19,7 @@ public class Main {
 		final Path path = Paths.get(args[0]);
 		//final Path path = Paths.get("models/Pizzas.dimacs");
 		String filename = path.getFileName().toString();
-		filename = filename.substring(0, filename.indexOf('.'));
+		filename = filename.substring(0, filename.lastIndexOf('.'));
 		
 		final IFeatureModel featureModel = FeatureModelManager.load(path);
 		if (featureModel != null) {
@@ -26,9 +28,11 @@ public class Main {
 			final FeatureModelAnalyzer analyzer = formula.getAnalyzer();
 			//analyzer.analyzeFeatureModel(null);
 			//System.out.println("Feature model is " + (analyzer.isValid(null) ? "not " : "") + "void");
-			System.out.println("Core features: " + analyzer.getCoreFeatures(null));
-			System.out.println("Dead features: " + analyzer.getDeadFeatures(null));
+			List<IFeature> cores = analyzer.getCoreFeatures(null);
+			List<IFeature> dead = analyzer.getDeadFeatures(null);
 			long elapsedTime = System.nanoTime() - startTime;
+			System.out.println("Core features: " + cores.size() + cores);
+			System.out.println("Dead features: " + dead.size() + dead);
 			double seconds = elapsedTime / 1e9;
 			System.out.println("Model;Tool;SAT-solver;Seconds");
 			System.out.println(filename + ";FeatureIDE;Sat4j;" + seconds);
