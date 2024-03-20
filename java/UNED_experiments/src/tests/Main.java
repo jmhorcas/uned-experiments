7e371a386a65165de6bc0209eb2ef3932f49d913
José Miguel Horcas <horcas@uma.es>
2024-03-20 17:38:13 +0100

    Modify FeaturEIDE exec

--- java/UNED_experiments/src/tests/Main.java
+++ java/UNED_experiments/src/tests/Main.java
@@ -2,10 +2,13 @@ package tests;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import de.ovgu.featureide.fm.core.FeatureModelAnalyzer;
+import de.ovgu.featureide.fm.core.analysis.cnf.formula.FMAnalyzerCreator;
 import de.ovgu.featureide.fm.core.analysis.cnf.formula.FeatureModelFormula;
+import de.ovgu.featureide.fm.core.analysis.cnf.solver.RuntimeTimeoutException;
 import de.ovgu.featureide.fm.core.base.IFeature;
 import de.ovgu.featureide.fm.core.base.IFeatureModel;
 import de.ovgu.featureide.fm.core.init.FMCoreLibrary;
@@ -24,18 +27,34 @@ public class Main {
 		final IFeatureModel featureModel = FeatureModelManager.load(path);
 		if (featureModel != null) {
 			FeatureModelFormula formula = new FeatureModelFormula(featureModel);
+			
 			long startTime = System.nanoTime();
-			final FeatureModelAnalyzer analyzer = formula.getAnalyzer();
-			//analyzer.analyzeFeatureModel(null);
-			//System.out.println("Feature model is " + (analyzer.isValid(null) ? "not " : "") + "void");
-			List<IFeature> cores = analyzer.getCoreFeatures(null);
-			List<IFeature> dead = analyzer.getDeadFeatures(null);
+			List<IFeature> cores = new ArrayList<>();
+			List<IFeature> dead = new ArrayList<>();
+			boolean timeout = false;
+			try {
+				final FeatureModelAnalyzer analyzer = formula.getAnalyzer();
+				//analyzer.analyzeFeatureModel(null);
+				//System.out.println("Feature model is " + (analyzer.isValid(null) ? "not " : "") + "void");
+				cores = analyzer.getCoreFeatures(null);
+				dead = analyzer.getDeadFeatures(null);
+			} catch (RuntimeTimeoutException e) {
+				e.printStackTrace();
+				timeout = true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			long elapsedTime = System.nanoTime() - startTime;
 			System.out.println("Core features: " + cores.size() + cores);
 			System.out.println("Dead features: " + dead.size() + dead);
 			double seconds = elapsedTime / 1e9;
 			System.out.println("Model;Tool;SAT-solver;Seconds");
-			System.out.println(filename + ";FeatureIDE;Sat4j;" + seconds);
+			if (timeout) {
+				System.out.println(filename + ";FeatureIDE;Sat4j;timeout");
+			} else {
+				System.out.println(filename + ";FeatureIDE;Sat4j;" + seconds);
+			}
+			
 		} else {
 			System.out.println("Feature model could not be read!");
 		}
